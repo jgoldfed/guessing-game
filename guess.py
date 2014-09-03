1841a1da52530134b6e52347bdb7129342221365
JGoldfed <JGoldfed@hotmail.com>
2014-09-03 17:44:20 -0400

    Added a guess of 5 times

--- guess.py
+++ guess.py
@@ -3,13 +3,17 @@ from random import randint
 def main():
     print "Welcome to the number guessing game!"
     number=randint(1,10)
+    i=1
     while True:
-        guess=raw_input("Guess a number between one and ten: ")
+        if i>5:
+            break
+        guess=raw_input("Try #" + str(i) + ": Guess a number between one and ten: ")
         if int(guess)==number:
             print "You got it!"
             break
         else:
             print "That's incorrect."
+            i+=1
 
 
 if __name__=='__main__':
